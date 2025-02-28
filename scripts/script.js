13778c98be68e647428039b60295254c49a14277
Syrgaks <majrambekovsyrgak@gmail.com>
2025-03-01 02:26:26 +0600

    g

--- scripts/script.js
+++ scripts/script.js
@@ -26,14 +26,14 @@ const carField = document.getElementById('car');
 const nameField = document.getElementById('name');
 const phoneField = document.getElementById('phone');
 
-// Обработчик для кнопки с id "order-action"
+
 document.getElementById('order-action').addEventListener('click', function () {
-    // Собираем поля в массив
+    
     const fields = [carField, nameField, phoneField];
 
     let hasError = false;
 
-    // Проверяем поля циклом
+    
     fields.forEach((field) => {
         if (field.value.trim() === '') {
             field.style.borderColor = 'red';
@@ -43,17 +43,17 @@ document.getElementById('order-action').addEventListener('click', function () {
         }
     });
 
-    // Дополнительная проверка для номера телефона
+    
     if (phoneField.value.trim().length < 10) {
         phoneField.style.borderColor = 'red';
         hasError = true;
     }
 
-    // Если все поля валидны
+
     if (!hasError) {
         alert('Спасибо за заявку! Мы скоро свяжемся с вами');
 
-        // Очищаем поля
+        
         fields.forEach((field) => (field.value = ''));
     }
 });
